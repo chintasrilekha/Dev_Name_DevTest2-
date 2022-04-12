0e8221dae6076932a3bc19dc771a069ff0fd6438
chintasrilekha <74331549+chintasrilekha@users.noreply.github.com>
2022-04-12 05:55:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).on('ready', function() {
+    $(".center").slick({
+      dots: true,
+    //   infinite: true,
+      centerMode: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      centerMode: true,
+      arrows: true
+    });
+  });
